0a6504af25afca10379d2434db4d037e764fb51f
Adam Epstein <Labuser@wtk316-14.cec.wustl.edu>
2016-01-28 19:14:16 -0600

    Finished Instagram Lab for Week 1

---
+++ Instagram Feed/InstagramCell.swift
@@ -0,0 +1,25 @@
+//
+//  InstagramCell.swift
+//  Instagram Feed
+//
+//  Created by Labuser on 1/28/16.
+//  Copyright © 2016 wustl. All rights reserved.
+//
+
+import UIKit
+
+class InstagramCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Instagram Feed/PhotoViewController.swift
+++ Instagram Feed/PhotoViewController.swift
@@ -7,13 +7,22 @@
 //
 
 import UIKit
+import AFNetworking
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var data:[NSDictionary]?
 
+    @IBOutlet weak var TableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        TableView.rowHeight = 320
+        
+        TableView.dataSource = self
+        TableView.delegate = self
 
         // Do any additional setup after loading the view.
         let clientId = "e05c462ebd86446ea48a5af73769b602"
@@ -32,6 +41,7 @@ class PhotoViewController: UIViewController {
                         data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
                             self.data = responseDictionary["data"] as! [NSDictionary]
+                            self.TableView.reloadData()
                     }
                 }
         });
@@ -43,6 +53,73 @@ class PhotoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("photoCell", forIndexPath: indexPath) as! InstagramCell
+        
+        let info = data![indexPath.section]
+        let images = info["images"] as! NSDictionary
+        let resolution = images["standard_resolution"] as! NSDictionary
+        let photoURL = resolution["url"] as! String
+
+        
+        let imageURL = NSURL(string: photoURL)
+        
+        cell.photoView.setImageWithURL(imageURL!)
+        cell.photoView.frame.size = CGSizeMake(320, 320)
+        
+        
+        
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if let data = data {
+            return data.count
+        } else{
+            return 0
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
+
+        let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = 15;
+        profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).CGColor
+        profileView.layer.borderWidth = 1;
+        
+        // Use the section number to get the right URL
+        let info = data![section]
+        let user = info["user"] as! NSDictionary
+        let user_pic = user["profile_picture"] as! String
+
+        let profileURL = NSURL(string: user_pic)
+        print(profileURL)
+        profileView.setImageWithURL(profileURL!)
+        
+        headerView.addSubview(profileView)
+        
+        // Add a UILabel for the username here
+        let userLabel = UILabel(frame: CGRect(x: 50, y: 10, width: 260, height: 30))
+        userLabel.text = user["username"] as! String
+        
+        headerView.addSubview(userLabel)
+        
+        return headerView
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
 
     /*
     // MARK: - Navigation
